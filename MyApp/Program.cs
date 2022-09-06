a2c18b82745afcc17e8e34eca76b46534441aa71
Noah Gift <noah.gift@gmail.com>
2022-09-06 19:26:39 +0000

    copilot enabled dotnet

---
+++ MyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿//build a hello world program that prints out two numbers added together
+//the numbers are 5 and 6
+//the output should be 11
+
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 6;
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+    }
+}
